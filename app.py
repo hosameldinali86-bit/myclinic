3157eb283d07ecec4e01bc241a73726c466f3920
hosameldinali86-bit <hosameldinali86@gmail.com>
2026-03-22 02:00:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api_key = st.sidebar.text_input("Enter Gemini API Key", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 
     # Main Clinical Interface
     st.header("Patient Presentation")
